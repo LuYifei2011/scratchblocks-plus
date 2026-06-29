6745ee0219881f3b994b27d15b1732ef872c206e
Lu Yifei <118034848+LuYifei2011@users.noreply.github.com>
2026-06-29 13:55:40 +0800

    reset snapshots to match upstream/main

--- snapshots/index.js
+++ snapshots/index.js
@@ -1,4 +1,10 @@
+import headless from "./headless.js"
 import { runTests } from "./runner.js"
 import {} from "./tests.js"
+;(async () => {
+  await headless.start()
 
-runTests()
+  await runTests(headless)
+
+  await headless.stop()
+})()
